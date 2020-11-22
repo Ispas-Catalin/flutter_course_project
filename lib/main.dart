da62362fc78b53f149a161ffac9f04bd12986ea4
Ispas Catalin <ispascatalin66@gmail.com>
2020-11-22 15:10:26 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'src/0.1.dart' as app;
+
+void main() {
+  app.main();
+}
---
+++ lib/src/0.1.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+double res = 50.00;
+double _visible = 0.0;
+String hintText = 'enter the amount in EUR';
+String errorText;
+String resValue;
+
+ScrollController _scrollController = ScrollController();
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _scrollController,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(8.0, 0.0, 0.0, 0.0),
+              child: Image.asset(
+                'assets/images/lei.jpg',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width * 6 / 8,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(12.0),
+              child: TextField(
+                keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true) ,
+                onChanged: (String value) {
+                  _scrollController.animateTo(
+                    _scrollController.position.maxScrollExtent,
+                    curve: Curves.easeOut,
+                    duration: const Duration(milliseconds: 200),
+                  );
+                  resValue = value;
+                },
+                decoration: InputDecoration(
+                  hintText: '$hintText',
+                  errorText: errorText,
+                ),
+              ),
+            ),
+            RaisedButton(
+                child: Text('Convert!'),
+                onPressed: () {
+                  setState(() {
+                    if (double.tryParse(resValue) != null) {
+                      errorText = null;
+                      _visible = 1.0;
+                      res = double.parse(resValue) * 4.5;
+                    } else {
+                      errorText = 'please enter a number';
+                      _visible = 0.0;
+                    }
+                  });
+                }),
+            Opacity(
+              opacity: _visible,
+              child: Text(
+                res.toStringAsFixed(2) + ' RON',
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.grey,
+
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
